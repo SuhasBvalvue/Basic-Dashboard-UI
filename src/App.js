dafa49517144171cb3e3e881054e927e553d93aa
SuhasBvalvue <68270146+SuhasBvalvue@users.noreply.github.com>
2020-07-16 11:33:51 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,154 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        
+      <div id="a">
+             <img src="logo.png" id="imglg" />
+      </div>
+      <div id="txtlg">
+          BTPL
+      </div>
+
+
+
+
+    <div id="b">
+       <div  id="notification"><div class="dropdown">
+       <button class="dropbtn1"><img src="notification.png" alt="HTML tutorial" style={{height:'0.5cm'}}/></button>
+       <div class="dropdown-content">
+          <a href="#">Link 1</a>
+          <a href="#">Link 2</a>
+          <a href="#">Link 3</a>
+        </div>
+       </div>
+       </div>
+
+      <div id="admin">
+             <img src="admin.jpg" id="imglg2" />
+      </div>
+    
+       <div id="txtlg2">
+          <a href="default.asp" target="_blank">Jane Pearson</a>
+        
+           <div style={{color: "grey"}}>Administrator</div>
+            
+        </div>
+    
+       </div>
+       <hr className="1" />
+      </header> 
+
+      <div id="btn">
+         <div class="dropdown">
+            <button class="dropbtn">Home</button>
+            <div class="dropdown-content">
+            <a href="#">Link 1</a>
+            <a href="#">Link 2</a>
+            <a href="#">Link 3</a>
+            </div>
+         </div>
+
+      <div class="dropdown">
+            <button class="dropbtn">Interface</button>
+            <div class="dropdown-content">
+            <a href="#">Link 1</a>
+            <a href="#">Link 2</a>
+            <a href="#">Link 3</a>
+      </div>
+      </div>
+      <div class="dropdown">
+            <button class="dropbtn">Components</button>
+            <div class="dropdown-content">
+            <a href="#">Link 1</a>
+            <a href="#">Link 2</a>
+            <a href="#">Link 3</a>
+      </div>
+      </div>
+      <div class="dropdown">
+            <button class="dropbtn">Pages</button>
+            <div class="dropdown-content">
+            <a href="#">Link 1</a>
+            <a href="#">Link 2</a>
+            <a href="#">Link 3</a>
+      </div>
+      </div>  
+      <div class="dropdown">
+            <button class="dropbtn">Forms</button>
+            <div class="dropdown-content">
+            <a href="#">Link 1</a>
+            <a href="#">Link 2</a>
+            <a href="#">Link 3</a>
+      </div>
+      </div>
+      <div class="dropdown">
+            <button class="dropbtn">Gallery</button>
+            <div class="dropdown-content">
+            <a href="#">Link 1</a>
+            <a href="#">Link 2</a>
+            <a href="#">Link 3</a>
+      </div>
+      </div>
+
+      <div class="dropdown">
+            <button class="dropbtn">Documentation</button>
+            <div class="dropdown-content">
+            <a href="#">Link 1</a>
+            <a href="#">Link 2</a>
+            <a href="#">Link 3</a>
+      </div>
+      </div>
+      </div>
+
+            
+
+      <div id="window">
+        
+        <div id="name">
+           <h3>Dashboard</h3>      
+        </div>
+      <div class="float-container">
+
+        
+  
+       <div class="float-child">
+        <div class="blue">Float Column 2</div>
+       </div>
+       <div class="float-child">
+        <div class="blue">Float Column 2</div>
+       </div>
+       <div class="float-child">
+        <div class="blue">Float Column 2</div>
+       </div>
+       <div class="float-child">
+        <div class="blue">Float Column 2</div>
+       </div>
+       <div class="float-child">
+        <div class="blue">Float Column 2</div>
+       </div>
+       <div class="float-child">
+        <div class="blue">Float Column 2</div>
+       </div>
+  
+</div>
+
+           
+  
+       <div class="element">
+          <div class="blue">Float Column 2</div>
+        
+      </div>
+      <div class="element">
+          <div class="blue">Float Column 2</div>
+        
+      </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
